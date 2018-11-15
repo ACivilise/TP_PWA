0756e775961a0d0f4a20c8e335f392e51c40aed6
ACivilise <coucouccoouu@hotmail.com>
2018-11-15 22:02:49 +0100

    mise en place du style

--- src/components/options/options.component.ts
+++ src/components/options/options.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-options',
-    templateUrl: 'options.component.html'
+    templateUrl: './options.component.html'
 })
 export class OptionsComponent {
 }
