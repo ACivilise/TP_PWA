345556b4652cbeb3a8d10fa385bf7975070c3f01
ACivilise <coucouccoouu@hotmail.com>
2018-11-15 21:05:03 +0100

    mise en place logos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -7,13 +8,35 @@ import { environment } from '../environments/environment';
 
 // Components
 import { HomeComponent } from '../components/home/home.component';
+import { OptionsComponent } from '../components/options/options.component';
+
+// Routes
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'options',      component: OptionsComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    OptionsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
